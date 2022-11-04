8b37d51483c8047d4394af3f96cd9b860cd128d1
Theordius <87307483+Theordius@users.noreply.github.com>
2022-11-04 15:31:09 +0100

    Add logo to the screen

--- TaekwondoGPFTUiKit/Views/FirstScreen.swift
+++ TaekwondoGPFTUiKit/Views/FirstScreen.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class FirstScreen: UIViewController {
-
+    
+    let image = UIImage(named: "logo")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        mainLogo()
     }
-
-
+    
+    func mainLogo() {
+        let imageView = UIImageView(image: image!)
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 125),
+            imageView.widthAnchor.constraint(equalToConstant: 125)
+        ])
+    }
+    
 }
 
