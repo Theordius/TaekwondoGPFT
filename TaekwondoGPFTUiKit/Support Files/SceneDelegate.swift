19f506464926765f8fac50809d9b3091581f9107
Theordius <87307483+Theordius@users.noreply.github.com>
2022-11-04 20:48:59 +0100

    Add new constains and theory button

--- TaekwondoGPFTUiKit/Support Files/SceneDelegate.swift
+++ TaekwondoGPFTUiKit/Support Files/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = FirstScreen()
+        window?.rootViewController = UINavigationController(rootViewController: FirstScreen())
         window?.makeKeyAndVisible()
     }
 
--- TaekwondoGPFTUiKit/Views/FirstScreen.swift
+++ TaekwondoGPFTUiKit/Views/FirstScreen.swift
@@ -10,11 +10,15 @@ import UIKit
 class FirstScreen: UIViewController {
     
     let image = UIImage(named: "logo")
+    let theoryButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         mainLogo()
+        title = "Wybierz"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        theoryScreen()
     }
     
     func mainLogo() {
@@ -24,11 +28,37 @@ class FirstScreen: UIViewController {
         
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            imageView.heightAnchor.constraint(equalToConstant: 125),
-            imageView.widthAnchor.constraint(equalToConstant: 125)
+            imageView.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 225),
+            imageView.heightAnchor.constraint(equalToConstant: 100),
+            imageView.widthAnchor.constraint(equalToConstant: 100)
         ])
     }
     
+    func theoryScreen() {
+        view.addSubview(theoryButton)
+        
+        theoryButton.configuration = .filled()
+        theoryButton.configuration?.baseBackgroundColor = .systemBlue
+        theoryButton.configuration?.title = "Teoria"
+        
+        theoryButton.addTarget(self, action: #selector(goToTheoryScreen), for: .touchUpInside)
+        
+        theoryButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            theoryButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            theoryButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            theoryButton.heightAnchor.constraint(equalToConstant: 50),
+            theoryButton.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    @objc func goToTheoryScreen() {
+        let theoryScreen = TheoryView()
+        theoryScreen.title = "Teoria"
+        navigationController?.pushViewController(theoryScreen, animated: true)
+    }
+    
 }
+
 
