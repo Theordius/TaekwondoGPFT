5fde00e26c0279efd73a868bd37381491b4f5d03
Theordius <87307483+Theordius@users.noreply.github.com>
2023-03-17 09:31:25 +0100

    Create location data model and location.json file to be implemented into map section on the main screen.

---
+++ TaekwondoGPFT/ViewModels/Models/LocationModel.swift
@@ -0,0 +1,23 @@
+//
+//  LocationModel.swift
+//  TaekwondoGPFT
+//
+//  Created by Rafał Gęsior on 17/03/2023.
+//
+
+import Foundation
+import MapKit
+
+struct SectionLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
+
