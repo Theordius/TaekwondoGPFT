1ca42583c2281c712a47d4cc78fc16ec8e0797f8
Theordius <87307483+Theordius@users.noreply.github.com>
2023-03-17 09:50:00 +0100

    Create custom Map Annotation

--- TaekwondoGPFT/App/MapView.swift
+++ TaekwondoGPFT/App/MapView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct MapView: View {
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(Gradient(colors: [.gray, .white]))
-                .ignoresSafeArea()
+            if #available(iOS 16.0, *) {
+                Rectangle()
+                    .fill(Gradient(colors: [.gray, .white]))
+                    .ignoresSafeArea()
+            } else {
+                // Fallback on earlier versions
+            }
             Text("NASZE ODDZIAŁY")
             
         }
---
+++ TaekwondoGPFT/Views/MapAnnotationView.swift
@@ -0,0 +1,52 @@
+//
+//  MapAnnotationView.swift
+//  TaekwondoGPFT
+//
+//  Created by Rafał Gęsior on 17/03/2023.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    
+    //MARK: - PROPERTIES
+    
+    var location: SectionLocation
+    @State private var animation: Double = 0.0
+    
+    //MARK: - BODY
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.clear)
+                .frame(width: 54, height: 54, alignment: .center)
+            
+            Circle()
+                .stroke(Color.gray, lineWidth: 2)
+                .frame(width: 52, height: 52, alignment: .center)
+                .scaleEffect(1 + CGFloat(animation))
+                .opacity(1 - animation)
+            
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48, alignment: .center)
+                .clipShape(Circle())
+        } //: ZSTACK
+        .onAppear {
+            withAnimation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false)) {
+                animation = 1
+            }
+        }
+    }
+}
+
+//MARK: - PREVIEW
+struct MapAnnotationView_Previews: PreviewProvider {
+    static var locations: [SectionLocation] = Bundle.main.decode("Locations.json")
+    static var previews: some View {
+        MapAnnotationView(location: locations[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
